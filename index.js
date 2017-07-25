e1f97e77db07549a7bc5447cfd75caa743d336a8
David Granstrom <david@davidgranstrom.com>
2017-07-25 23:51:40 +0200

    Print current track to console

---
+++ index.js
@@ -0,0 +1,17 @@
+const moment = require('moment');
+const OSC = require('osc-js');
+const PORT = 9912;
+const options = { port: PORT };
+const osc = new OSC({ plugin: new OSC.DatagramPlugin(options) });
+
+osc.on('open', () => {
+  console.log('Started osc server listening on port ${PORT}');
+});
+
+osc.on('/currentTrack', (message) => {
+  const info = message.args[0];
+  const now = moment().format('h:mm:ss');
+  console.log(`${now} | ${info}`);
+});
+
+osc.open({ port: PORT });
